4a93840b8505326b0e89c82dd3456ecde984373c
abyanKhairi <khairirisha4@gmail.com>
2025-07-31 08:38:38 +0700

    commit

---
+++ application/logs/log-2025-07-12.php
@@ -0,0 +1,38 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+INFO - 2025-07-12 05:58:51 --> Config Class Initialized
+INFO - 2025-07-12 05:58:51 --> Hooks Class Initialized
+DEBUG - 2025-07-12 05:58:51 --> UTF-8 Support Enabled
+INFO - 2025-07-12 05:58:51 --> Utf8 Class Initialized
+INFO - 2025-07-12 05:58:51 --> URI Class Initialized
+INFO - 2025-07-12 05:58:51 --> Router Class Initialized
+INFO - 2025-07-12 05:58:51 --> Output Class Initialized
+INFO - 2025-07-12 05:58:51 --> Security Class Initialized
+DEBUG - 2025-07-12 05:58:51 --> Global POST, GET and COOKIE data sanitized
+INFO - 2025-07-12 05:58:51 --> Input Class Initialized
+INFO - 2025-07-12 05:58:51 --> Language Class Initialized
+INFO - 2025-07-12 05:58:51 --> Loader Class Initialized
+INFO - 2025-07-12 05:58:51 --> Helper loaded: url_helper
+INFO - 2025-07-12 05:58:51 --> Helper loaded: file_helper
+INFO - 2025-07-12 05:58:51 --> Helper loaded: form_helper
+INFO - 2025-07-12 05:58:51 --> Helper loaded: html_helper
+INFO - 2025-07-12 05:58:51 --> Helper loaded: text_helper
+INFO - 2025-07-12 05:58:51 --> Helper loaded: array_helper
+INFO - 2025-07-12 05:58:51 --> Helper loaded: cookie_helper
+INFO - 2025-07-12 05:58:51 --> Database Driver Class Initialized
+INFO - 2025-07-12 05:58:51 --> Email Class Initialized
+DEBUG - 2025-07-12 05:58:51 --> Session: "sess_save_path" is empty; using "session.save_path" value from php.ini.
+INFO - 2025-07-12 05:58:51 --> Session: Class initialized using 'files' driver.
+INFO - 2025-07-12 05:58:51 --> Form Validation Class Initialized
+INFO - 2025-07-12 05:58:51 --> Controller Class Initialized
+INFO - 2025-07-12 05:58:51 --> Model "Thread_Model" initialized
+INFO - 2025-07-12 05:58:51 --> Language file loaded: language/english/pagination_lang.php
+INFO - 2025-07-12 05:58:51 --> Pagination Class Initialized
+DEBUG - 2025-07-12 05:58:51 --> Page: 1 | Offset: 0 | PerPage: 1 | TotalRows: 3
+DEBUG - 2025-07-12 05:58:51 --> LIMIT: 1 OFFSET: 0 | Search: 
+DEBUG - 2025-07-12 05:58:51 --> Jumlah thread dari getThreadsWithKomentar(): 1
+INFO - 2025-07-12 05:58:51 --> File loaded: C:\xampp\htdocs\Perpustakaan\application\views\Template/Header.php
+INFO - 2025-07-12 05:58:51 --> File loaded: C:\xampp\htdocs\Perpustakaan\application\views\Forum/Threads/Index.php
+INFO - 2025-07-12 05:58:51 --> File loaded: C:\xampp\htdocs\Perpustakaan\application\views\Template/Footer.php
+INFO - 2025-07-12 05:58:51 --> Final output sent to browser
+DEBUG - 2025-07-12 05:58:51 --> Total execution time: 0.0695
